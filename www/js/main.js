24179a26361b47d16774ba74bcf4962fb24b82c0
Justin Cornell <jcornell@acromediainc.com>
2017-11-01 21:53:06 -0500

    Initial commit

---
+++ www/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    addContaminationControlListeners();
+
+    function addContaminationControlListeners() {
+        var contaminationControls = document.getElementsByClassName('zone-contamination-op');
+
+        for (var i = 0; i < contaminationControls.length; i++) {
+
+            contaminationControls[i].addEventListener('click', function () {
+
+                var target = document.getElementById(this.dataset.controlTargetZone);
+                var targetContent = parseInt(target.innerHTML);
+                var op = this.dataset.op;
+
+                switch (op) {
+                    case 'increase':
+                        target.innerHTML = targetContent + 1;
+                        break;
+                    case 'decrease':
+                        if (targetContent > 0) {
+                            target.innerHTML = targetContent - 1;
+                        }
+                        break;
+                }
+            });
+        }
+    }
+});
